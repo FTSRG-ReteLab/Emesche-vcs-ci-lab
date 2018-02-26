eb86819de5084887289e0bbc5e74e3d986cfcac6
Your Name <you@example.com>
2018-02-26 08:24:43 -0800

    Tachograph.java added

---
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/Tachograph.java
@@ -0,0 +1,24 @@
+package hu.bme.mit.train.sensor;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+import java.sql.Time;
+import java.util.Date;
+
+
+public class Tachograph {
+    Table<Date, Integer, Integer> table;
+
+    public Tachograph(){
+        table = HashBasedTable.create();
+    }
+
+    public void addElement(Time ido, int position, int speed){
+        table.put(ido, position, speed);
+    }
+
+    public int getCount(){
+        return table.size();
+    }
+}
